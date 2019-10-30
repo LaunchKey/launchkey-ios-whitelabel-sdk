bf742013d9cbf8d03312ef4be3123f3896256932
Ani Shiranian <ani.shiranian@iovation.com>
2019-10-30 09:10:23 -0700

    - Removed the old Auth SDK and embedded the new Auth SDK (v4.9.0) in both Sample Apps

---
+++ SampleAppSwift/Authenticator.framework/Headers/Authenticator.h
@@ -0,0 +1,44 @@
+//
+//  Authenticator.h
+//  Authenticator
+//
+//  Created by Steven Gerhard on 4/24/19.
+//  Copyright © 2019 LaunchKey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
+
+//! Project version number for Authenticator.
+FOUNDATION_EXPORT double AuthenticatorVersionNumber;
+
+//! Project version string for Authenticator.
+FOUNDATION_EXPORT const unsigned char AuthenticatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Authenticator/PublicHeader.h>
+
+#import <Authenticator/AuthResponseButton.h>
+#import <Authenticator/AuthResponseNegativeButton.h>
+#import <Authenticator/AuthResponseExpirationTimerView.h>
+#import <Authenticator/AuthenticatorConfigurator.h>
+#import <Authenticator/AuthenticatorConfig.h>
+#import <Authenticator/AuthenticatorManager.h>
+#import <Authenticator/AuthRequestViewController.h>
+#import <Authenticator/AuthenticatorButton.h>
+#import <Authenticator/AuthorizationSlider.h>
+#import <Authenticator/AuthorizationSliderButton.h>
+#import <Authenticator/AuthRequestContainer.h>
+#import <Authenticator/IOATextField.h>
+#import <Authenticator/IOALabel.h>
+#import <Authenticator/IOADevice.h>
+#import <Authenticator/CircleCodeImageView.h>
+#import <Authenticator/AuthRequestManager.h>
+#import <Authenticator/DevicesViewController.h>
+#import <Authenticator/LKPolicy.h>
+#import <Authenticator/LocalAuthManager.h>
+#import <Authenticator/IOASession.h>
+#import <Authenticator/PinCodeButton.h>
+#import <Authenticator/SecurityFactorTableViewCell.h>
+#import <Authenticator/SessionsViewController.h>
+#import <Authenticator/LKSessionManager.h>
+#import <Authenticator/LKDeviceManager.h>
--- SampleAppSwift/SampleAppSwift-Bridging-Header.h
+++ SampleAppSwift/SampleAppSwift-Bridging-Header.h
@@ -3,24 +3,4 @@
 //
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
-#import <Authenticator/AuthenticatorManager.h>
-#import <Authenticator/AuthenticatorConfigurator.h>
-#import <Authenticator/AuthRequestManager.h>
-#import <Authenticator/SessionsViewController.h>
-#import <Authenticator/DevicesViewController.h>
-#import <Authenticator/LKSessionManager.h>
-#import <Authenticator/LKDeviceManager.h>
-#import <Authenticator/IOADevice.h>
-#import <Authenticator/IOASession.h>
-#import <Authenticator/IOATextField.h>
-#import <Authenticator/PinCodeButton.h>
-#import <Authenticator/CircleCodeImageView.h>
-#import <Authenticator/AuthenticatorButton.h>
-#import <Authenticator/AuthorizationSliderButton.h>
-#import <Authenticator/AuthorizationSlider.h>
-#import <Authenticator/SecurityFactorTableViewCell.h>
-#import <Authenticator/AuthRequestContainer.h>
-#import <Authenticator/LocalAuthManager.h>
-#import <Authenticator/AuthResponseButton.h>
-#import <Authenticator/AuthResponseNegativeButton.h>
-#import <Authenticator/AuthResponseExpirationTimerView.h>
+#import <Authenticator/Authenticator.h>
